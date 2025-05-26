360a96bb84c17bc79ec0c33d6589fb1331521103
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-05-26 09:13:12 +0100

    basic addition and subtraction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+add_result = number1 + number2
+sub_result = number1 - number2
+product_result = number1 * number2
+
+print (f"Addition of {number1} and {number2} is {add_result}")
+print (f"Subtraction of {number1} and {number2} is {sub_result}")
+print (f"Product of {number1} and {number2} is {product_result}")
