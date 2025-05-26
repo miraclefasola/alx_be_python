290e1717f72c6784783a91480f8a0cd37516bf4c
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-05-26 10:48:53 +0100

    added a simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#Calculating the simple interest earned on an investment over a period of 3 years.
+#Formula = Principal * Interest * Time
+P = 1000
+R = 0.01
+T = 3
+I = P*R*T
+
+Print (f "Product of {P} and {R} and {T} is {I}")
