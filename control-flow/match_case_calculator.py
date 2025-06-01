f430862c6592d92d25793075da880dee96812b2f
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-06-01 19:16:40 +0100

    output fix

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,11 +3,10 @@ num2 = int(input("Enter the second number:"))
 operation = input ("Choose the operation (+, -, *, /):")
 match operation:
     case "+"
-        print(f"{num1} + {num2}")
+        print(f"The result is {num1} + {num2}")
     case "-"
-        print(f"{num1} - {num2}")
+        print(f"The result is {num1} - {num2}")
     case "*"
-        print(f"{num1} * {num2}")
+        print(f" The result is {num1} * {num2}")
     case "/"
-        print (f"{num1}" * {num2})
-        
+        print (f"The result is {num1}" * {num2})
