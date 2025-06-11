541dd63d2f5bff3adbe40c953f943db3865f8608
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-06-11 17:02:16 +0100

    updated

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ import datetime
 
 def display_current_datetime():
     current_date = datetime.datetime.now()
-    current_date = current_date.strftime("%Y- %m- %d %H:%M:%S")
+    current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(current_date)
 display_current_datetime()
 
