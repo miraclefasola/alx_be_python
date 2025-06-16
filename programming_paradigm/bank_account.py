db1b7bf515ac4e971bb4ee9699038305d25fae1c
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-06-16 17:26:18 +0100

    bank app

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, deposit_amount):
+        self.account_balance += deposit_amount
+        return deposit_amount
+
+    def withdraw(self, withdraw_amount):
+
+        if withdraw_amount > self.account_balance:
+            return False
+        self.account_balance -= withdraw_amount
+        return True
+    def display_balance(self):
+        print (f"Current Balance: ${self.account_balance}")
