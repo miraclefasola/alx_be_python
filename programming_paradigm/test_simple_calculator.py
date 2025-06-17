c05638d818acf5f8cd30e07aa18610a6ff6616c5
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-06-17 17:03:56 +0100

    updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,24 +6,24 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
     def test_add(self):
         calc = SimpleCalculator()
-        result = calc.add(10, 20)
-        self.assertEqual(result,30)
+        test_addition = calc.add(10, 20)
+        self.assertEqual(test_addition,30)
         self.assertEqual(calc.add(-6, -2), -8)
 
     def test_subtract(self):
         calc = SimpleCalculator()
-        result = calc.subtract(10, 20)
-        self.assertEqual(result,-10)
+        test_subtraction = calc.subtract(10, 20)
+        self.assertEqual(test_subtraction,-10)
         self.assertEqual(calc.subtract(10, 15), -5)
 
     def test_multiply(self):
         calc = SimpleCalculator()
-        result = calc.multiply(10, 20)
-        self.assertEqual(result,200)
+        test_multiply = calc.multiply(10, 20)
+        self.assertEqual(test_multiply,200)
         self.assertEqual(calc.multiply(-6, -2), 12)
 
     def test_divide(self):
         calc = SimpleCalculator()
-        result = calc.divide(10, 20)
-        self.assertEqual(result,0.5)
+        test_divide = calc.divide(10, 20)
+        self.assertEqual(test_divide,0.5)
         self.assertEqual(calc.divide(6, 0), None)
